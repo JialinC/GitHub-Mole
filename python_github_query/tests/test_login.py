b11e13bb65e3096bfbb457b2010d33f0f5bec1f0
JialinC <cjl742952519@gmail.com>
2023-06-26 00:23:06 -0400

    move client fixture to its own file.

--- python_github_query/tests/test_login.py
+++ python_github_query/tests/test_login.py
@@ -1,23 +1,5 @@
-import os
 import pytest
-from python_github_query.github_graphql.authentication import PersonalAccessTokenAuthenticator
-from python_github_query.github_graphql.client import Client
 from python_github_query.queries.login import UserLoginViewer, UserLogin
-
-
-@pytest.fixture(scope="class")
-def graphql_client():
-    # Set up the GraphQL client
-    client = Client(
-        host="api.github.com", is_enterprise=False,
-        authenticator=PersonalAccessTokenAuthenticator(token=os.environ.get("GITHUB_PERSONAL_ACCESS_TOKEN"))
-    )
-
-    enterprise_client = Client(
-        host="github.ncsu.edu", is_enterprise=True,
-        authenticator=PersonalAccessTokenAuthenticator(token=os.environ.get("GITHUB_ENTERPRISE_PERSONAL_ACCESS_TOKEN"))
-    )
-    yield [client, enterprise_client]
 
 
 @pytest.mark.usefixtures("graphql_client")
