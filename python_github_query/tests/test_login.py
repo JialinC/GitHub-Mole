4cd47b8cd575f47967660a51b0ec8ca97da5e02c
JialinC <cjl742952519@gmail.com>
2023-06-25 23:49:11 -0400

    add test for login.py (UserLoginViewer and UserLogin) class.

--- python_github_query/tests/test_login.py
+++ python_github_query/tests/test_login.py
@@ -1,8 +1,8 @@
 import os
 import pytest
-from ..github_graphql.authentication import PersonalAccessTokenAuthenticator
-from ..github_graphql.client import Client
-from ..queries.login import UserLoginViewer, UserLogin
+from python_github_query.github_graphql.authentication import PersonalAccessTokenAuthenticator
+from python_github_query.github_graphql.client import Client
+from python_github_query.queries.login import UserLoginViewer, UserLogin
 
 
 @pytest.fixture(scope="class")
@@ -21,8 +21,8 @@ def graphql_client():
 
 
 @pytest.mark.usefixtures("graphql_client")
-class TestUserLogin:
-    def test_user_login_viewer(self, graphql_client):
+class TestLogin:
+    def test_user_login_viewer_public(self, graphql_client):
         client = graphql_client[0]
         response = client.execute(
             query=UserLoginViewer.query, substitutions={}
@@ -34,3 +34,49 @@ class TestUserLogin:
             }
         }
         assert response == expected_data
+
+    def test_user_login_viewer_enterprise(self, graphql_client):
+        client = graphql_client[1]
+        response = client.execute(
+            query=UserLoginViewer.query, substitutions={}
+        )
+
+        expected_data = {
+            'viewer': {
+                'login': 'jcui9'
+            }
+        }
+        assert response == expected_data
+
+    def test_user_login_public(self, graphql_client):
+        client = graphql_client[0]
+        response = client.execute(
+            query=UserLogin.query, substitutions={"user": "JialinC"}
+        )
+
+        expected_data = {
+            'user': {
+                'login': 'JialinC',
+                'name': 'Jialin Cui',
+                'email': '',
+                'createdAt': '2018-04-20T04:37:16Z'}
+        }
+        assert response == expected_data
+
+    def test_user_login_enterprise(self, graphql_client):
+        client = graphql_client[1]
+        response = client.execute(
+            query=UserLogin.query, substitutions={"user": "jcui9"}
+        )
+
+        expected_data = {
+            'user': {
+                'login': 'jcui9',
+                'name': 'jcui9',
+                'email': 'jcui9@ncsu.edu',
+                'createdAt': '2019-09-11T17:08:06Z'
+            }
+        }
+        assert response == expected_data
+
+
