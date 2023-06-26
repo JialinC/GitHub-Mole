c7f38962929f66a17ed15ad0cce5c64efeec6c67
JialinC <cjl742952519@gmail.com>
2023-06-26 19:59:38 -0400

    1. change relative import to absolute import. 2. change PaginatedQuery to Query also add documentation for the raw GraphQL query.

--- python_github_query/queries/commits.py
+++ python_github_query/queries/commits.py
@@ -1,16 +1,40 @@
-from github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
+from python_github_query.github_graphql.query import QueryNode, Query
 
 
 class UserCommits:
-    query = PaginatedQuery(
+    """
+    The raw GraphQL query:
+    query ($user: String!, $start: DateTime!, $end: DateTime!) {
+        user(login: "$user"){
+            contributionsCollection(from:"$start",to:"$end"){
+                startedAt
+                endedAt
+                hasActivityInThePast
+                hasAnyContributions
+                hasAnyRestrictedContributions
+                restrictedContributionsCount
+                totalCommitContributions
+                totalIssueContributions
+                totalPullRequestContributions
+                totalPullRequestReviewContributions
+                totalRepositoriesWithContributedCommits
+                totalRepositoriesWithContributedIssues
+                totalRepositoriesWithContributedPullRequestReviews
+                totalRepositoriesWithContributedPullRequests
+                totalRepositoryContributions
+            }
+        }
+    }
+    """
+    query = Query(
         fields=[
             QueryNode(
                 "user",
                 args={"login": "$user"},
                 fields=[
-                    QueryNodePaginator(
+                    QueryNode(
                         "contributionsCollection",
-                        args={"start": "$start", "end": "$end"},
+                        args={"from": "$start", "to": "$end"},
                         fields=[
                             "startedAt",
                             "endedAt",
@@ -33,3 +57,19 @@ class UserCommits:
             )
         ]
     )
+
+
+
+
+
+
+
+
+
+
+#
+# {"user" : "JialinC",
+# "start" : "2022-02-07T18:11:20Z",
+# "end" : "2021-02-07T18:11:20Z"}
+#
+# "2018-04-20T04:37:16Z"
