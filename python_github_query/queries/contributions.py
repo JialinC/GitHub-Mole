741d1e6441125f60dd6ea12e9740860fe6be7491
JialinC <cjl742952519@gmail.com>
2023-07-01 02:19:24 -0400

    minor change

--- python_github_query/queries/contributions.py
+++ python_github_query/queries/contributions.py
@@ -1,26 +1,33 @@
-from github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
+from python_github_query.github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
 
 
-class UserContributions:
-    query = PaginatedQuery(
-        fields=[
-            QueryNode(
-                "user",
-                args={"login": "$user"},
-                fields=[
-                    "login",
-                    QueryNodePaginator(
-                        "$contribution_type",
-                        fields=[
-                            QueryNode(
-                                "nodes",
-                                fields=["createdAt"]
-                            )
-                        ]
-                    )
-                ]
-            )
-        ]
-    )
+class UserContributions(PaginatedQuery):
+    def __init__(self):
+        super().__init__(
+            fields=[
+                QueryNode(
+                    "user",
+                    args={"login": "$user"},
+                    fields=[
+                        "login",
+                        QueryNodePaginator(
+                            "$contribution_type",
+                            args={"first": "$pg_size"},
+                            fields=[
+                                "totalCount",
+                                QueryNode(
+                                    "nodes",
+                                    fields=["createdAt"]
+                                ),
+                                QueryNode(
+                                    "pageInfo",
+                                    fields=["endCursor", "hasNextPage"]
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
 
 
