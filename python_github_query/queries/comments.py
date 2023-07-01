902f16fcbd3d42576b4577044469842f2c52f2a6
JialinC <cjl742952519@gmail.com>
2023-07-01 02:19:33 -0400

    minor change

--- python_github_query/queries/comments.py
+++ python_github_query/queries/comments.py
@@ -2,29 +2,30 @@ from python_github_query.github_graphql.query import QueryNode, PaginatedQuery, 
 
 
 class UserComments(PaginatedQuery):
-    query = PaginatedQuery(
-        fields=[
-            QueryNode(
-                "user",
-                args={"login": "$user"},
-                fields=[
-                    "login",
-                    QueryNodePaginator(
-                        "$comment_type",
-                        args={"first": "$pg_size"},
-                        fields=[
-                            "totalCount",
-                            QueryNode(
-                                "nodes",
-                                fields=["body", "createdAt"]
-                            ),
-                            QueryNode(
-                                "pageInfo",
-                                fields=["endCursor", "hasNextPage"]
-                            )
-                        ]
-                    )
-                ]
-            )
-        ]
-    )
+    def __init__(self):
+        super().__init__(
+            fields=[
+                QueryNode(
+                    "user",
+                    args={"login": "$user"},
+                    fields=[
+                        "login",
+                        QueryNodePaginator(
+                            "$comment_type",
+                            args={"first": "$pg_size"},
+                            fields=[
+                                "totalCount",
+                                QueryNode(
+                                    "nodes",
+                                    fields=["body", "createdAt"]
+                                ),
+                                QueryNode(
+                                    "pageInfo",
+                                    fields=["endCursor", "hasNextPage"]
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
