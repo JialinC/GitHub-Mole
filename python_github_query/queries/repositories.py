6faee7e7ede86c727f003d6c1add71d94aea304c
JialinC <cjl742952519@gmail.com>
2023-07-01 02:19:07 -0400

    minor change

--- python_github_query/queries/repositories.py
+++ python_github_query/queries/repositories.py
@@ -1,55 +1,59 @@
-from github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
+from python_github_query.github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
 
 
-class UserRepositories:
-    query = PaginatedQuery(
-        fields=[
-            QueryNode(
-                "user",
-                args={"login": "$user"},
-                fields=[
-                    QueryNodePaginator(
-                        "repositories",
-                        page_length=10,
-                        fields=[
-                            QueryNode(
-                                "nodes",
-                                fields=[
-                                    "name",
-                                    "isEmpty",
-                                    "createdAt",
-                                    "updatedAt",
-                                    "forkCount",
-                                    "stargazerCount",
-                                    QueryNode("watchers", fields=["totalCount"]),
-                                    QueryNode("primaryLanguage", fields=["name"]),
-                                    QueryNode(
-                                        "languages",
-                                        args={"first": 100},
-                                        fields=[
-                                            "totalSize",
-                                            "totalCount",
-                                            QueryNode(
-                                                "edges",
-                                                fields=[
-                                                    "size",
-                                                    QueryNode("node", fields=["name"])
-                                                ]
-                                            )
-                                        ]
-                                    ),
-                                ]
-                            )
-                        ]
-                    ),
-                ]
-            )
-        ]
-    )
+class UserRepositories(PaginatedQuery):
+    def __init__(self):
+        super().__init__(
+            fields=[
+                QueryNode(
+                    "user",
+                    args={"login": "$user"},
+                    fields=[
+                        QueryNodePaginator(
+                            "repositories",
+                            args={"first": "$pg_size",
+                                  "isFork": "$is_fork",
+                                  "ownerAffiliations": "$ownership",
+                                  "orderBy": "$order_by"},
+                            fields=[
+                                QueryNode(
+                                    "nodes",
+                                    fields=[
+                                        "name",
+                                        "isEmpty",
+                                        "createdAt",
+                                        "updatedAt",
+                                        "forkCount",
+                                        "stargazerCount",
+                                        QueryNode("watchers", fields=["totalCount"]),
+                                        QueryNode("primaryLanguage", fields=["name"]),
+                                        QueryNode(
+                                            "languages",
+                                            args={"first": 100,
+                                                  "orderBy": {"field": "SIZE",
+                                                              "direction": "DESC"}},
+                                            fields=[
+                                                "totalSize",
+                                                "totalCount",
+                                                QueryNode(
+                                                    "edges",
+                                                    fields=[
+                                                        "size",
+                                                        QueryNode("node", fields=["name"])
+                                                    ]
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                QueryNode(
+                                    "pageInfo",
+                                    fields=["endCursor", "hasNextPage"]
+                                )
+                            ]
+                        ),
+                    ]
+                )
+            ]
+        )
 
-    def __init__(self, is_fork: bool, owner_affiliations: list):
-        self.query.fields[0].fields[0].args.update({
-            "isFork": is_fork,
-            "ownerAffiliations": owner_affiliations,
-            "orderBy": {"field": "CREATED_AT", "direction": "ASC"}
-        })
