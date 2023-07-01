7fa6892f0c7a26a68fd880186edc4b20441b6201
JialinC <cjl742952519@gmail.com>
2023-07-01 02:20:07 -0400

    update debug script.

--- python_github_query/debug_script.py
+++ python_github_query/debug_script.py
@@ -9,6 +9,8 @@ from python_github_query.queries.login import UserLoginViewer, UserLogin
 from python_github_query.queries.metrics import UserMetrics
 from python_github_query.queries.commits import UserCommits
 from python_github_query.queries.comments import UserComments
+from python_github_query.queries.contributions import UserContributions
+from python_github_query.queries.repositories import UserRepositories
 
 import requests
 from requests import Response
@@ -29,12 +31,127 @@ if __name__ == '__main__':
     path2 = "https://github.ncsu.edu/api/graphql"
     header2 = enterprise_client._generate_headers()
 
+    args = {"user": "JialinC", "pg_size": 2, "comment_type": "issueComments"}
+    response = client.execute(
+        query=UserLoginViewer(), substitutions={}
+    )
+    print(response)
 
-    test = UserComments.query
+    test = UserCommits()
+    print(test)
+    # print(Query.convert_dict())
+    # args = {"user": "JialinC", "pg_size": 5, "is_fork": True, "ownership": "OWNER", "order_by": {"field": "CREATED_AT", "direction": "ASC"}}
+    # converted = Query.convert_dict(args)
+    print(test.substitute(**{"user": "jcui9", "start": "2020-02-07T18:11:20Z", "end": "2021-02-07T18:11:20Z"}))
+
+    # pullRequests, issues, gists, repositoryDiscussions
+    # print("True", "OWNER")
+    # for response in client.execute(query=UserRepositories(), substitutions={"user": "JialinC",
+    #                                                                         "pg_size": 5,
+    #                                                                         "is_fork": True,
+    #                                                                         "ownership": "OWNER",
+    #                                                                         "order_by": {"field": "CREATED_AT", "direction": "ASC"}}):
+    #     print(response)
+    #
+    # print("True", "COLLABORATOR")
+    # for response in client.execute(query=UserRepositories(), substitutions={"user": "JialinC",
+    #                                                                         "pg_size": 5,
+    #                                                                         "is_fork": True,
+    #                                                                         "ownership": "COLLABORATOR",
+    #                                                                         "order_by": {"field": "CREATED_AT",
+    #                                                                                      "direction": "ASC"}}):
+    #     print(response)
+    #
+    # print("False", "OWNER")
+    # for response in client.execute(query=UserRepositories(), substitutions={"user": "JialinC",
+    #                                                                         "pg_size": 5,
+    #                                                                         "is_fork": False,
+    #                                                                         "ownership": "OWNER",
+    #                                                                         "order_by": {"field": "CREATED_AT",
+    #                                                                                      "direction": "ASC"}}):
+    #     print(response)
+    #
+    # print("False", "COLLABORATOR")
+    # for response in client.execute(query=UserRepositories(), substitutions={"user": "JialinC",
+    #                                                                         "pg_size": 5,
+    #                                                                         "is_fork": False,
+    #                                                                         "ownership": "COLLABORATOR",
+    #                                                                         "order_by": {"field": "CREATED_AT",
+    #                                                                                      "direction": "ASC"}}):
+    #     print(response)
+
+    # print("True", "OWNER")
+    # for response in enterprise_client.execute(query=UserRepositories(), substitutions={"user": "jcui9",
+    #                                                                         "pg_size": 5,
+    #                                                                         "is_fork": True,
+    #                                                                         "ownership": "OWNER",
+    #                                                                         "order_by": {"field": "CREATED_AT",
+    #                                                                                      "direction": "ASC"}}):
+    #     print(response)
+    #
+    # print("True", "COLLABORATOR")
+    # for response in enterprise_client.execute(query=UserRepositories(), substitutions={"user": "jcui9",
+    #                                                                         "pg_size": 5,
+    #                                                                         "is_fork": True,
+    #                                                                         "ownership": "COLLABORATOR",
+    #                                                                         "order_by": {"field": "CREATED_AT",
+    #                                                                                      "direction": "ASC"}}):
+    #     print(response)
+    #
+    # print("False", "OWNER")
+    # for response in enterprise_client.execute(query=UserRepositories(), substitutions={"user": "jcui9",
+    #                                                                         "pg_size": 5,
+    #                                                                         "is_fork": False,
+    #                                                                         "ownership": "OWNER",
+    #                                                                         "order_by": {"field": "CREATED_AT",
+    #                                                                                      "direction": "ASC"}}):
+    #     print(response)
+    #
+    # print("False", "COLLABORATOR")
+    # for response in enterprise_client.execute(query=UserRepositories(), substitutions={"user": "jcui9",
+    #                                                                         "pg_size": 5,
+    #                                                                         "is_fork": False,
+    #                                                                         "ownership": "COLLABORATOR",
+    #                                                                         "order_by": {"field": "CREATED_AT",
+    #                                                                                      "direction": "ASC"}}):
+    #     print(response)
+
+
+
+
+    # for response in client.execute(query=UserContributions(),
+    #                                substitutions={"user": "JialinC", "pg_size": 2, "contribution_type": "gists"}):
+    #     print(response)
+    # for response in client.execute(query=UserContributions(),
+    #                                substitutions={"user": "JialinC", "pg_size": 2, "contribution_type": "repositoryDiscussions"}):
+    #     print(response)
+    # for response in enterprise_client.execute(query=UserRepositories, substitutions={"user": "jcui9", "pg_size": 2,
+    #                                                                          "contribution_type": "pullRequests"}):
+    #     print(response)
+    # for response in enterprise_client.execute(query=UserContributions(),
+    #                                substitutions={"user": "jcui9", "pg_size": 2, "contribution_type": "issues"}):
+    #     print(response)
+    # for response in enterprise_client.execute(query=UserContributions(),
+    #                                substitutions={"user": "jcui9", "pg_size": 2, "contribution_type": "gists"}):
+    #     print(response)
+    # for response in enterprise_client.execute(query=UserContributions(),
+    #                                substitutions={"user": "jcui9", "pg_size": 2,
+    #                                               "contribution_type": "repositoryDiscussions"}):
+    #     print(response)
+
+
+
+
+
+
+
+
+
+    # test = UserComments.query
     # print(test)
     # print(test.path)
     # print(test.paginator)
-    args = {"user": "JialinC", "pg_size": 2, "comment_type": "issueComments"}
+    # args = {"user": "JialinC", "pg_size": 2, "comment_type": "issueComments"}
     # path = PaginatedQuery.extract_path_to_pageinfo_node(test)
     # print(path)
     # pnode = path[1]
@@ -78,12 +195,40 @@ if __name__ == '__main__':
     #    headers=header1
     # ).json()['data']
     # print(response)
+    # response = client.execute(
+    #     query=UserLoginViewer(), substitutions={}
+    # )
+    # print(response)
 
-    for response in client.execute(query=UserComments.query, substitutions={"user": "JialinC", "pg_size": 2, "comment_type": "issueComments"}):
-        print(response)
+    # for response in client.execute(query=UserComments(), substitutions={"user": "JialinC", "pg_size": 2, "comment_type": "commitComments"}):
+    #     print(response)
+    # for response in client.execute(query=UserComments(),
+    #                                substitutions={"user": "JialinC", "pg_size": 2, "comment_type": "gistComments"}):
+    #     print(response)
+    # for response in client.execute(query=UserComments(),
+    #                                substitutions={"user": "JialinC", "pg_size": 2, "comment_type": "issueComments"}):
+    #     print(response)
+    # for response in client.execute(query=UserComments(),
+    #                                substitutions={"user": "JialinC", "pg_size": 2, "comment_type": "repositoryDiscussionComments"}):
+    #     print(response)
 
-    for response in client.execute(query=UserComments.query, substitutions={"user": "JialinC", "pg_size": 2, "comment_type": "issueComments"}):
-        print(response)
+    # for response in enterprise_client.execute(query=UserComments(), substitutions={"user": "jcui9", "pg_size": 2, "comment_type": "commitComments"}):
+    #     print(response)
+    # for response in enterprise_client.execute(query=UserComments(),
+    #                                substitutions={"user": "jcui9", "pg_size": 2, "comment_type": "gistComments"}):
+    #     print(response)
+    # for response in enterprise_client.execute(query=UserComments(),
+    #                                substitutions={"user": "jcui9", "pg_size": 2, "comment_type": "issueComments"}):
+    #     print(response)
+    # for response in enterprise_client.execute(query=UserComments(),
+    #                                substitutions={"user": "jcui9", "pg_size": 2, "comment_type": "repositoryDiscussionComments"}):
+    #     print(response)
+
+    # for response in enterprise_client.execute(query=UserComments(), substitutions={"user": "jcui9", "pg_size": 2, "comment_type": "commitComments"}):
+    #     print(response)
+
+    # for response in client.execute(query=UserComments(), substitutions={"user": "JialinC", "pg_size": 2, "comment_type": "issueComments"}):
+    #     print(response)
 
 
 
