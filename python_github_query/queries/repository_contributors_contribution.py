528155eedbc400794eebc71953400851cb301bf9
JialinC <cjl742952519@gmail.com>
2023-07-05 21:13:53 -0400

    added new query classes RepositoryContributors and RepositoryContributorsContribution. Also added related tests.

---
+++ python_github_query/queries/repository_contributors_contribution.py
@@ -0,0 +1,53 @@
+from python_github_query.github_graphql.query import QueryNode, Query
+
+
+class RepositoryContributorsContribution(Query):
+    def __init__(self):
+        super().__init__(
+            fields=[
+                QueryNode(
+                    "repository",
+                    args={"owner": "$owner",
+                          "name": "$repo_name"},
+                    fields=[
+                        QueryNode(
+                            "defaultBranchRef",
+                            fields=[
+                                QueryNode(
+                                    "target",
+                                    fields=[
+                                        QueryNode(
+                                            "... on Commit",
+                                            fields=[
+                                                QueryNode(
+                                                    "history",
+                                                    args={"author": "$id"},
+                                                    fields=[
+                                                        "totalCount",
+                                                        QueryNode(
+                                                            "nodes",
+                                                            fields=[
+                                                                "authoredDate",
+                                                                "changedFilesIfAvailable",
+                                                                "additions",
+                                                                "deletions",
+                                                                QueryNode(
+                                                                    "parents (first: 2)",
+                                                                    fields=[
+                                                                        "totalCount"
+                                                                    ]
+                                                                )
+                                                            ]
+                                                        )
+                                                    ]
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
