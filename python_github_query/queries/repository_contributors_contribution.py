78c5dece66e34764ff53c2e1a4edc6bd2299df80
JialinC <cjl742952519@gmail.com>
2023-07-08 01:45:25 -0400

    add class method user_cumulated_contribution to calculate contributor's cumulative stats.

--- python_github_query/queries/repository_contributors_contribution.py
+++ python_github_query/queries/repository_contributors_contribution.py
@@ -51,3 +51,25 @@ class RepositoryContributorsContribution(Query):
                 )
             ]
         )
+
+    @staticmethod
+    def user_cumulated_contribution(raw_data: dict):
+        """
+        Return the cumulated contribution of the contributor
+        Args:
+            raw_data: the raw data returned by the query
+        Returns:
+            list: a list of contributor's total additions, total deletions, and total number of commits.
+        """
+        nodes = raw_data['repository']['defaultBranchRef']['target']['history']['nodes']
+        total_additions = 0
+        total_deletions = 0
+        total_commits = 0
+        for node in nodes:
+            if node['parents'] and node['parents']['totalCount'] < 2:
+                total_additions += node['additions']
+                total_deletions += node['deletions']
+                total_commits += 1
+            else:
+                continue
+        return [total_commits, total_additions, total_deletions]
