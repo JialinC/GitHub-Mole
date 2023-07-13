336ec77bba02d0683df1364fdc7a6d6426a0401f
JialinC <cjl742952519@gmail.com>
2023-07-13 01:32:16 -0400

    reconstruct the package structure and add class methods.

--- python_github_query/queries/comments/user_commit_comments.py
+++ python_github_query/queries/comments/user_commit_comments.py
@@ -1,4 +1,5 @@
 from python_github_query.github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
+import python_github_query.util.helper as helper
 
 
 class UserCommitComments(PaginatedQuery):
@@ -17,7 +18,7 @@ class UserCommitComments(PaginatedQuery):
                                 "totalCount",
                                 QueryNode(
                                     "nodes",
-                                    fields=["body", "createdAt"]
+                                    fields=["createdAt"]
                                 ),
                                 QueryNode(
                                     "pageInfo",
@@ -29,3 +30,31 @@ class UserCommitComments(PaginatedQuery):
                 )
             ]
         )
+
+    @staticmethod
+    def user_commit_comments(raw_data: dict):
+        """
+        Return the contributors contribution collection
+        Args:
+            raw_data: the raw data returned by the query
+        Returns:
+        """
+        commit_comments = raw_data["user"]["commitComments"]["nodes"]
+        return commit_comments
+
+    @staticmethod
+    def created_before_time(commit_comments: list, time: str):
+        """
+        Return the contributors contribution collection
+        Args:
+            commit_comments: the raw data returned by the query
+            time:
+        Returns:
+        """
+        counter = 0
+        for commit_comment in commit_comments:
+            if helper.created_before(commit_comment["createdAt"], time):
+                counter += 1
+            else:
+                break
+        return counter
--- python_github_query/queries/comments/user_gist_comments.py
+++ python_github_query/queries/comments/user_gist_comments.py
@@ -1,4 +1,5 @@
 from python_github_query.github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
+import python_github_query.util.helper as helper
 
 
 class UserGistComments(PaginatedQuery):
@@ -17,7 +18,7 @@ class UserGistComments(PaginatedQuery):
                                 "totalCount",
                                 QueryNode(
                                     "nodes",
-                                    fields=["body", "createdAt"]
+                                    fields=["createdAt"]
                                 ),
                                 QueryNode(
                                     "pageInfo",
@@ -29,3 +30,32 @@ class UserGistComments(PaginatedQuery):
                 )
             ]
         )
+
+    @staticmethod
+    def user_gist_comments(raw_data: dict):
+        """
+        Return the contributors contribution collection
+        Args:
+            raw_data: the raw data returned by the query
+        Returns:
+        """
+        gist_comments = raw_data["user"]["gistComments"]["nodes"]
+        return gist_comments
+
+    @staticmethod
+    def created_before_time(gist_comments: list, time: str):
+        """
+        Return the contributors contribution collection
+        Args:
+            gist_comments: the raw data returned by the query
+            time:
+        Returns:
+        """
+        counter = 0
+        for gist_comment in gist_comments:
+            if helper.created_before(gist_comment["createdAt"], time):
+                counter += 1
+            else:
+                break
+        return counter
+
