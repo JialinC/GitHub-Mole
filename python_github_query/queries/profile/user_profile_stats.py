a41e8176faba0d15b94f369b55e04687acff8131
JialinC <cjl742952519@gmail.com>
2023-07-15 02:21:11 -0400

    add class method to formulate query result.

--- python_github_query/queries/profile/user_profile_stats.py
+++ python_github_query/queries/profile/user_profile_stats.py
@@ -27,16 +27,42 @@ class UserProfileStats(Query):
                         QueryNode("following", fields=["totalCount"]),
                         QueryNode("followers", fields=["totalCount"]),
                         QueryNode("gists", fields=["totalCount"]),
-                        QueryNode("gistComments", fields=["totalCount"]),
-                        QueryNode("issueComments", fields=["totalCount"]),
                         QueryNode("issues", fields=["totalCount"]),
                         QueryNode("projects", fields=["totalCount"]),
                         QueryNode("pullRequests", fields=["totalCount"]),
                         QueryNode("repositories", fields=["totalCount"]),
-                        QueryNode("repositoryDiscussionComments", fields=["totalCount"]),
                         QueryNode("repositoryDiscussions", fields=["totalCount"]),
+                        QueryNode("gistComments", fields=["totalCount"]),
+                        QueryNode("issueComments", fields=["totalCount"]),
+                        QueryNode("commitComments", fields=["totalCount"]),
+                        QueryNode("repositoryDiscussionComments", fields=["totalCount"]),
                     ]
                 )
             ]
         )
+
+    @staticmethod
+    def profile_stats(profile_stats: dict):
+        """
+        Return the contributors contribution collection
+        Args:
+            profile_stats: the raw data returned by the query
+        Returns:
+        """
+        raw_data = profile_stats["user"]
+        profile_stats = {"github": raw_data["login"],
+                         "created_at": raw_data["createdAt"],
+                         "company": raw_data["company"],
+                         "followers": raw_data["followers"]["totalCount"],
+                         "gists": raw_data["gists"]["totalCount"],
+                         "issues": raw_data["issues"]["totalCount"],
+                         "projects": raw_data["projects"]["totalCount"],
+                         "pull_requests": raw_data["pullRequests"]["totalCount"],
+                         "repositories": raw_data["repositories"]["totalCount"],
+                         "repository_discussions": raw_data["repositoryDiscussions"]["totalCount"],
+                         "gist_comments": raw_data["gistComments"]["totalCount"],
+                         "issue_comments": raw_data["issueComments"]["totalCount"],
+                         "commit_comments": raw_data["commitComments"]["totalCount"],
+                         "repository_discussion_comments": raw_data["repositoryDiscussionComments"]["totalCount"]}
+        return profile_stats
 
