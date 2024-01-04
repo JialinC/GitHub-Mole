501c13c5e936e9d0e30afb8b71a14e998d013e14
JialinC <cjl742952519@gmail.com>
2024-01-04 17:09:02 -0500

    1. add comments for user_pull_requests 2. add tests for user_pull_requests

--- backend/app/services/github_query/queries/contributions/user_pull_requests.py
+++ backend/app/services/github_query/queries/contributions/user_pull_requests.py
@@ -1,8 +1,17 @@
-from github_query.github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
-
+from typing import List, Dict
+from backend.app.services.github_query.github_graphql.query import QueryNode, PaginatedQuery, QueryNodePaginator
+import backend.app.services.github_query.utils.helper as helper
 
 class UserPullRequests(PaginatedQuery):
-    def __init__(self):
+    """
+    UserPullRequests extends PaginatedQuery to fetch pull requests associated with a specific user.
+    It navigates through potentially large sets of pull request data with pagination.
+    """
+    
+    def __init__(self) -> None:
+        """
+        Initializes the UserPullRequests query with necessary fields and pagination support.
+        """
         super().__init__(
             fields=[
                 QueryNode(
@@ -29,3 +38,38 @@ class UserPullRequests(PaginatedQuery):
                 )
             ]
         )
+
+    @staticmethod
+    def user_pull_requests(raw_data: Dict) -> List[Dict]:
+        """
+        Extracts pull requests from the raw data returned by a GraphQL query.
+
+        Args:
+            raw_data (Dict): The raw data returned from the GraphQL query.
+
+        Returns:
+            List[Dict]: A list of pull requests, each represented as a dictionary.
+        """
+        pull_requests = raw_data.get("user", {}).get("pullRequests", {}).get("nodes", [])
+        return pull_requests
+
+    @staticmethod
+    def created_before_time(pull_requests: List[Dict], time: str) -> int:
+        """
+        Counts the number of pull requests created before a specified time.
+
+        Args:
+            pull_requests (List[Dict]): A list of pull requests, each represented as a dictionary.
+            time (str): The time string to compare each pull request's creation time against.
+
+        Returns:
+            int: The count of pull requests created before the specified time.
+        """
+        counter = 0
+        for pull_request in pull_requests:
+            if helper.created_before(pull_request.get("createdAt", ""), time):
+                counter += 1
+            else:
+                break
+        return counter
+
