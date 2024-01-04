2435d72bf5110fee1ab41ac8e50673647ebf0f0e
JialinC <cjl742952519@gmail.com>
2024-01-04 14:13:39 -0500

    update comments for authentication class.

--- backend/app/services/github_query/github_graphql/authentication.py
+++ backend/app/services/github_query/github_graphql/authentication.py
@@ -1,28 +1,44 @@
+from typing import Dict
+
 class Authenticator:
     """
-    Handles Authentication Method for GitHub clients.
+    Authenticator is an abstract base class for handling different types of 
+    authentication methods for GitHub clients. It provides a common interface 
+    for authentication by defining a method to get the authorization header.
     """
-    def get_authorization_header(self):
+    def get_authorization_header(self) -> Dict[str, str]:
+        """
+        Abstract method to get the authorization header. Implementations of this 
+        method in subclasses should return the necessary header for authentication 
+        based on the specific method they represent.
+
+        Raises:
+            NotImplementedError: If the subclass does not implement this method.
+        """
         raise NotImplementedError("Authenticator cannot be implemented")
 
 
 class PersonalAccessTokenAuthenticator(Authenticator):
     """
-    Handles Personal Access Token Authentication Method for GitHub clients
+    PersonalAccessTokenAuthenticator is a concrete implementation of the Authenticator class,
+    providing authentication functionality specifically using a personal access token for GitHub.
     """
     def __init__(self, token: str):
         """
-        Sets the Authentication Personal Access Token.
+        Initializes the authenticator with a personal access token.
+
         Args:
-            token: Personal Access Token
+            token (str): The personal access token used for authentication.
         """
         self._token = token
 
-    def get_authorization_header(self):
+    def get_authorization_header(self) -> Dict[str, str]:
         """
-        Returns the authentication header.
+        Constructs and returns the authorization header using the personal access token.
+
         Returns:
-            Authentication Header
+            dict: A dictionary representing the authorization header required for 
+                  authentication with the GitHub API using a personal access token.
         """
         return {
             "Authorization": f"token {self._token}"
