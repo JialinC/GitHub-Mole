783479291a656e422bdada153c409bee21220186
JialinC <cjl742952519@gmail.com>
2024-01-27 01:12:06 -0500

    set up oauth.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,15 @@
-# from flask import Flask
-# from .api.github_api import github_api
+from flask import Flask
+from .auth.config import Config
+from .auth.oauth import config_oauth
+from .auth.oauth_routes import auth_bp
 
-# def create_app():
-#     app = Flask(__name__)
-#     app.register_blueprint(github_api)
-#     return app
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)  # Load configurations
+    app.debug = app.config.get('DEBUG', False)
+
+    config_oauth(app)  # Initialize OAuth with app configuration
+
+    app.register_blueprint(auth_bp, url_prefix='/auth')  # Register the Blueprint
+
+    return app
---
+++ backend/app/auth/config.py
@@ -0,0 +1,5 @@
+class Config(object):
+    DEBUG = True  # Ensure debug is enabled in your configuration for development
+    SECRET_KEY = 'your_secret_key_here'  # Consider using environment variables
+    GITHUB_OAUTH_CLIENT_ID = '918ef50cd94282d71b1b'
+    GITHUB_OAUTH_CLIENT_SECRET = '9d8242fe2293c6d8784f48ee6ba6117d1cb4e748'
