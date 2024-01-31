57d1dc6ae1a6fea72091d3285979d145c9c94481
JialinC <cjl742952519@gmail.com>
2024-01-31 18:27:59 -0500

    db related update

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask
-from .auth.config import Config
+from flask_migrate import Migrate
+from .database import db
+from .config import Config, AuthConfig, DBConfig
 from .auth.oauth import config_oauth
 from .auth.oauth_routes import auth_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)  # Load configurations
+    # Load configurations
+    app.config.from_object(Config)
+    app.config.from_object(AuthConfig)
+    app.config.from_object(DBConfig)
     app.debug = app.config.get('DEBUG', False)
 
     config_oauth(app)  # Initialize OAuth with app configuration
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    # Import models to ensure they are registered with SQLAlchemy
+    from .models import user, github_user_data
 
     app.register_blueprint(auth_bp, url_prefix='/auth')  # Register the Blueprint
 
--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,35 @@
 from app import create_app
+from app.database import db
+from sqlalchemy import text
+from flask import Flask, session
 
 app = create_app()
+
+@app.route('/index')
+def index():
+    print(session)
+    return 'This is the index page'
+
+@app.route('/show_session')
+def show_session():
+    # Print all session data to the console
+    for key, value in session.items():
+        print(f'{key}: {value}')
+    
+    return 'Session data printed to console.'
+
+@app.route('/test_db')
+def test_db():
+    try:
+        # Acquire a connection and execute "SELECT 1"
+        with db.engine.connect() as connection:
+            result = connection.execute(text('SELECT VERSION()'))
+            version = result.fetchone()[0]
+            return f'Successfully connected to MySQL database. Version: {version}'
+    except Exception as e:
+        # If an error occurs, it means the connection was unsuccessful
+        return f'Failed to connect to database. Error: {e}'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -9,129 +38,129 @@ if __name__ == "__main__":
 
 
 
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import os
-# G2370
-from github_query.github_graphql.authentication import PersonalAccessTokenAuthenticator
-from github_query.github_graphql.client import Client
-from github_query.queries.comments.user_commit_comments import UserCommitComments
-from github_query.queries.comments.user_gist_comments import UserGistComments
-from github_query.queries.comments.user_issue_comments import UserIssueComments
-from github_query.queries.comments.user_repository_discussion_comments import UserRepositoryDiscussionComments
-from github_query.queries.profile.user_login import UserLogin
-from github_query.queries.profile.user_profile_stats import UserProfileStats
-from github_query.queries.time_range_contributions.user_contributions_collection import UserContributionsCollection
-# G2371
-from github_query.model.authentication import PersonalAccessTokenAuthenticator
-from github_query.github_graphql.github_client import GitHubClient
-from github_query.queries.contributions.user_login import UserLogin
-from flask_server.service.paginated_service import PaginatedService
-# G2372
-from github_query.github_graphql.authentication import PersonalAccessTokenAuthenticator
-from github_query.github_graphql.client import Client
-from github_query.queries.repositories.user_login import UserLogin
-from github_query.queries.repositories.repository_commits import RepositoryCommits
-from github_query.queries.repositories.repository_contributors_contribution import RepositoryContributorsContribution
-from github_query.queries.repositories.repository_contributors import RepositoryContributors
+# from flask import Flask, jsonify, request
+# from flask_cors import CORS
+# import os
+# # G2370
+# from github_query.github_graphql.authentication import PersonalAccessTokenAuthenticator
+# from github_query.github_graphql.client import Client
+# from github_query.queries.comments.user_commit_comments import UserCommitComments
+# from github_query.queries.comments.user_gist_comments import UserGistComments
+# from github_query.queries.comments.user_issue_comments import UserIssueComments
+# from github_query.queries.comments.user_repository_discussion_comments import UserRepositoryDiscussionComments
+# from github_query.queries.profile.user_login import UserLogin
+# from github_query.queries.profile.user_profile_stats import UserProfileStats
+# from github_query.queries.time_range_contributions.user_contributions_collection import UserContributionsCollection
+# # G2371
+# from github_query.model.authentication import PersonalAccessTokenAuthenticator
+# from github_query.github_graphql.github_client import GitHubClient
+# from github_query.queries.contributions.user_login import UserLogin
+# from flask_server.service.paginated_service import PaginatedService
+# # G2372
+# from github_query.github_graphql.authentication import PersonalAccessTokenAuthenticator
+# from github_query.github_graphql.client import Client
+# from github_query.queries.repositories.user_login import UserLogin
+# from github_query.queries.repositories.repository_commits import RepositoryCommits
+# from github_query.queries.repositories.repository_contributors_contribution import RepositoryContributorsContribution
+# from github_query.queries.repositories.repository_contributors import RepositoryContributors
 
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# Allow all origins to access routes with '/api/' prefix
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+# # Allow all origins to access routes with '/api/' prefix
+# CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-auth_token = os.environ["GITHUB_PERSONAL_ACCESS_TOKEN"]
+# auth_token = os.environ["GITHUB_PERSONAL_ACCESS_TOKEN"]
 
-# Initialize GitHub client
-client = Client(
-    host="api.github.com", is_enterprise=False,
-    authenticator=PersonalAccessTokenAuthenticator(token=auth_token)
-)
+# # Initialize GitHub client
+# client = Client(
+#     host="api.github.com", is_enterprise=False,
+#     authenticator=PersonalAccessTokenAuthenticator(token=auth_token)
+# )
 
-@app.route('/api/github/userlogin')
-def fetch_github_data():
-    user_name = request.args.get("user")
-    response = client.execute(
-        query=UserLogin(), substitutions={"user": user_name}
-    )
-    return response
+# @app.route('/api/github/userlogin')
+# def fetch_github_data():
+#     user_name = request.args.get("user")
+#     response = client.execute(
+#         query=UserLogin(), substitutions={"user": user_name}
+#     )
+#     return response
 
-@app.route('/api/github/commitcomments')
-def fetch_commit_comments():
-    account_name = request.args.get("account_name")
-    page_size = request.args.get("page_size")
-    response = client.execute(
-        query=UserCommitComments(), 
-        substitutions={"user": account_name, "pg_size": page_size})
-    commit_comments = []
-    for commitComments in response:
-        commit_comments.append(commitComments)
+# @app.route('/api/github/commitcomments')
+# def fetch_commit_comments():
+#     account_name = request.args.get("account_name")
+#     page_size = request.args.get("page_size")
+#     response = client.execute(
+#         query=UserCommitComments(), 
+#         substitutions={"user": account_name, "pg_size": page_size})
+#     commit_comments = []
+#     for commitComments in response:
+#         commit_comments.append(commitComments)
 
-    return commit_comments
+#     return commit_comments
 
-@app.route('/api/github/gistcomments')
-def fetch_gist_comments():
-    account_name = request.args.get("account_name")
-    page_size = request.args.get("page_size")
-    response = client.execute(
-        query=UserGistComments(), 
-        substitutions={"user": account_name, "pg_size": page_size})
-    gist_comments = []
-    for gistComments in response:
-        gist_comments.append(gistComments)
+# @app.route('/api/github/gistcomments')
+# def fetch_gist_comments():
+#     account_name = request.args.get("account_name")
+#     page_size = request.args.get("page_size")
+#     response = client.execute(
+#         query=UserGistComments(), 
+#         substitutions={"user": account_name, "pg_size": page_size})
+#     gist_comments = []
+#     for gistComments in response:
+#         gist_comments.append(gistComments)
 
-    return gist_comments
+#     return gist_comments
 
-@app.route('/api/github/issuecomments')
-def fetch_issue_comments():
-    account_name = request.args.get("account_name")
-    page_size = request.args.get("page_size")
-    response = client.execute(
-        query=UserIssueComments(), 
-        substitutions={"user": account_name, "pg_size": page_size})
-    issue_comments = []
-    for issueComments in response:
-        issue_comments.append(issueComments)
+# @app.route('/api/github/issuecomments')
+# def fetch_issue_comments():
+#     account_name = request.args.get("account_name")
+#     page_size = request.args.get("page_size")
+#     response = client.execute(
+#         query=UserIssueComments(), 
+#         substitutions={"user": account_name, "pg_size": page_size})
+#     issue_comments = []
+#     for issueComments in response:
+#         issue_comments.append(issueComments)
 
-    return issue_comments
-
-
-@app.route('/api/github/repodiscussion')
-def fetch_user_repo_discussions():
-    account_name = request.args.get("account_name")
-    page_size = request.args.get("page_size")
-    response = client.execute(query=UserRepositoryDiscussionComments(),
-                              substitutions={"user": account_name, "pg_size": page_size})
-    repo_discussions = []
-    for discussion in response:
-        repo_discussions.append(discussion)
-
-    return repo_discussions
+#     return issue_comments
 
 
-@app.route('/api/github/profilestats')
-def fetch_user_profile_stats():
-    account_name = request.args.get("account_name")
-    response = client.execute(query=UserProfileStats(),
-                              substitutions={"user": account_name})
-    return UserProfileStats.profile_stats(response)
+# @app.route('/api/github/repodiscussion')
+# def fetch_user_repo_discussions():
+#     account_name = request.args.get("account_name")
+#     page_size = request.args.get("page_size")
+#     response = client.execute(query=UserRepositoryDiscussionComments(),
+#                               substitutions={"user": account_name, "pg_size": page_size})
+#     repo_discussions = []
+#     for discussion in response:
+#         repo_discussions.append(discussion)
+
+#     return repo_discussions
 
 
-@app.route('/api/github/contributioncollection')
-def fetch_user_contribution_collection():
-    account_name = request.args.get("account_name")
-    start_date = request.args.get("start_date")
-    end_date = request.args.get("end_date")
-    # "start": "2023-09-07T18:11:20Z"
-    # "end": "2023-10-17T18:11:20Z"
-    response = client.execute(query=UserContributionsCollection(), substitutions={"user": account_name,
-                                                                                  "start": start_date,
-                                                                                  "end": end_date})
-    return response
+# @app.route('/api/github/profilestats')
+# def fetch_user_profile_stats():
+#     account_name = request.args.get("account_name")
+#     response = client.execute(query=UserProfileStats(),
+#                               substitutions={"user": account_name})
+#     return UserProfileStats.profile_stats(response)
 
 
-# Run the app on 0.0.0.0
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=1234)
+# @app.route('/api/github/contributioncollection')
+# def fetch_user_contribution_collection():
+#     account_name = request.args.get("account_name")
+#     start_date = request.args.get("start_date")
+#     end_date = request.args.get("end_date")
+#     # "start": "2023-09-07T18:11:20Z"
+#     # "end": "2023-10-17T18:11:20Z"
+#     response = client.execute(query=UserContributionsCollection(), substitutions={"user": account_name,
+#                                                                                   "start": start_date,
+#                                                                                   "end": end_date})
+#     return response
+
+
+# # Run the app on 0.0.0.0
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=1234)
