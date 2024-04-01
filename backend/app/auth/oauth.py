55aa8f2dd420d2329e1ff4d75c2e87edbe65d973
JialinC <cjl742952519@gmail.com>
2024-03-31 23:51:54 -0400

    1. reformat the code to align with PEP8 2. add documentation

--- backend/app/auth/oauth.py
+++ backend/app/auth/oauth.py
@@ -1,23 +1,33 @@
+"""This module set up necessary configuration for OAuth."""
+
 from flask import url_for
 from authlib.integrations.flask_client import OAuth
 
 oauth = OAuth()
 
+
 def config_oauth(app):
+    """
+    The config_oauth function initializes the OAuth client with the Flask app instance and
+    registers GitHub as an OAuth provider using the app's configuration settings.
+    It sets up the necessary URLs and scopes required for OAuth authentication with GitHub.
+    """
     oauth.init_app(app)
 
     def get_redirect_uri():
         # Use url_for inside a function to ensure it's called within a request context
-        return url_for('auth.authorize', _external=True, _scheme='https')
+        return url_for("auth.authorize", _external=True, _scheme="https")
 
     oauth.register(
-        name='github',
-        client_id=app.config['GITHUB_OAUTH_CLIENT_ID'],
-        client_secret=app.config['GITHUB_OAUTH_CLIENT_SECRET'],
-        access_token_url='https://github.com/login/oauth/access_token',
-        authorize_url='https://github.com/login/oauth/authorize',
-        api_base_url='https://api.github.com/',
-        client_kwargs={'scope': 'user:email'}, # extend this to meet project requirements
+        name="github",
+        client_id=app.config["GITHUB_OAUTH_CLIENT_ID"],
+        client_secret=app.config["GITHUB_OAUTH_CLIENT_SECRET"],
+        access_token_url="https://github.com/login/oauth/access_token",
+        authorize_url="https://github.com/login/oauth/authorize",
+        api_base_url="https://api.github.com/",
+        client_kwargs={
+            "scope": "user:email"
+        },  # extend this to meet project requirements
         # Pass the function get_redirect_uri as the redirect_uri argument
-        redirect_uri=get_redirect_uri
+        redirect_uri=get_redirect_uri,
     )
