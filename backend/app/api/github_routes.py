83f4439bd5f98ceddeed16d85140ad4734f34823
JialinC <cjl742952519@gmail.com>
2024-03-31 22:56:30 -0400

    1. reformat the code to align with PEP8 2. add documentations to API endpoints

--- backend/app/api/github_routes.py
+++ backend/app/api/github_routes.py
@@ -1,15 +1,36 @@
-from flask import Blueprint, jsonify, request
+"""This file defines the URLs for various GraphQL endpoints."""
+
+from flask import Blueprint, jsonify
+
 # Import service methods
-from backend.app.services.github_graphql_services import get_current_user_login, get_specific_user_login
+from backend.app.services.github_graphql_services import (
+    get_current_user_login,
+    get_specific_user_login,
+)
 
-github_bp = Blueprint('api', __name__)
+github_bp = Blueprint("api", __name__)
 
-@github_bp.route('/graphql/current-user-login', methods=['GET'])
+
+@github_bp.route("/graphql/current-user-login", methods=["GET"])
 def current_user_login():
+    """
+    Route: /graphql/current-user-login
+    Method: GET
+    Description: Fetches the login details of the current authenticated user.
+    Returns: A JSON object containing the login information of the current user.
+    """
     data = get_current_user_login()
     return jsonify(data)
 
-@github_bp.route('/graphql/user-login/<username>', methods=['GET'])
+
+@github_bp.route("/graphql/user-login/<username>", methods=["GET"])
 def specific_user_login(username):
+    """
+    Route: /graphql/user-login/<username>
+    Method: GET
+    URL Parameter: username - The GitHub username of the user for whom to fetch login details.
+    Description: Retrieves the login details for a specific GitHub user identified by their username.
+    Returns: A JSON object containing the login information of the specified user.
+    """
     data = get_specific_user_login(username)
     return jsonify(data)
