a95d4af6a5ee671408606be1f663d02f9adc66e0
JialinC <cjl742952519@gmail.com>
2024-04-01 14:47:26 -0400

    1. reformat code to align with PEP8 2. add documentation

--- backend/app/models/github_user_data.py
+++ backend/app/models/github_user_data.py
@@ -1,9 +1,18 @@
+"""The module defines the GitHubUserData class, which holds the GitHub metrics collected for a given GitHub ID."""
+
 from datetime import datetime
 from app.database import db
 
+
 class GitHubUserData(db.Model):
+    """
+    The GitHubUserData class is a model that represents GitHub user data within the application.
+    It is designed to store various metrics related to a GitHub user's activity, such as contributions, commits, issues,
+    and more. This class is an ORM model that integrates with a database through Flask-SQLAlchemy.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     github_login = db.Column(db.String(100), nullable=False)
     semester = db.Column(db.String(100), nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -55,55 +64,58 @@ class GitHubUserData(db.Model):
 
     # Convert object properties to a dictionary
     def to_dict(self):
+        """
+        Converts the properties of the GitHubUserData instance into a dictionary, making it easier to serialize and
+        work with, especially when sending data in responses in a web application context.
+
+        Returns:
+            A dictionary representation of the instance with keys corresponding to attribute names and
+            values corresponding to the attribute values. Dates and times are converted to ISO format strings.
+        """
         return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'github_login': self.github_login,
-            'semester': self.semester,
-            'created_at': self.created_at.isoformat() if self.created_at else None,
-            'lifetime': self.lifetime,
-            'start_at': self.start_at.isoformat() if self.start_at else None,
-            'end_at': self.end_at.isoformat() if self.end_at else None,
-            'period': self.period,
-
-            'private_contributions': self.private_contributions,
-            'commits': self.commits,
-            'issues': self.issues,
-            'gists': self.gists,
-            'prs': self.prs,
-            'pr_reviews': self.pr_reviews,
-            'repository_discussions': self.repository_discussions,
-            'commit_comments': self.commit_comments,
-            'issue_comments': self.issue_comments,
-            'gist_comments': self.gist_comments,
-            'repository_discussion_comments': self.repository_discussion_comments,
-            'repos': self.repos,
-
-            'a_count': self.a_count,
-            'a_fork_count': self.a_fork_count,
-            'a_stargazer_count': self.a_stargazer_count,
-            'a_watcher_count': self.a_watcher_count,
-            'a_total_size': self.a_total_size,
-            'a_langs': self.a_langs,
-
-            'b_count': self.b_count,
-            'b_fork_count': self.b_fork_count,
-            'b_stargazer_count': self.b_stargazer_count,
-            'b_watcher_count': self.b_watcher_count,
-            'b_total_size': self.b_total_size,
-            'b_langs': self.b_langs,
-
-            'c_total_count': self.c_total_count,
-            'c_fork_count': self.c_fork_count,
-            'c_stargazer_count': self.c_stargazer_count,
-            'c_watcher_count': self.c_watcher_count,
-            'c_total_size': self.c_total_size,
-            'c_langs': self.c_langs,
-
-            'd_total_count': self.d_total_count,
-            'd_fork_count': self.d_fork_count,
-            'd_stargazer_count': self.d_stargazer_count,
-            'd_watcher_count': self.d_watcher_count,
-            'd_total_size': self.d_total_size,
-            'd_langs': self.d_langs,
+            "id": self.id,
+            "user_id": self.user_id,
+            "github_login": self.github_login,
+            "semester": self.semester,
+            "created_at": self.created_at.isoformat() if self.created_at else None,
+            "lifetime": self.lifetime,
+            "start_at": self.start_at.isoformat() if self.start_at else None,
+            "end_at": self.end_at.isoformat() if self.end_at else None,
+            "period": self.period,
+            "private_contributions": self.private_contributions,
+            "commits": self.commits,
+            "issues": self.issues,
+            "gists": self.gists,
+            "prs": self.prs,
+            "pr_reviews": self.pr_reviews,
+            "repository_discussions": self.repository_discussions,
+            "commit_comments": self.commit_comments,
+            "issue_comments": self.issue_comments,
+            "gist_comments": self.gist_comments,
+            "repository_discussion_comments": self.repository_discussion_comments,
+            "repos": self.repos,
+            "a_count": self.a_count,
+            "a_fork_count": self.a_fork_count,
+            "a_stargazer_count": self.a_stargazer_count,
+            "a_watcher_count": self.a_watcher_count,
+            "a_total_size": self.a_total_size,
+            "a_langs": self.a_langs,
+            "b_count": self.b_count,
+            "b_fork_count": self.b_fork_count,
+            "b_stargazer_count": self.b_stargazer_count,
+            "b_watcher_count": self.b_watcher_count,
+            "b_total_size": self.b_total_size,
+            "b_langs": self.b_langs,
+            "c_total_count": self.c_total_count,
+            "c_fork_count": self.c_fork_count,
+            "c_stargazer_count": self.c_stargazer_count,
+            "c_watcher_count": self.c_watcher_count,
+            "c_total_size": self.c_total_size,
+            "c_langs": self.c_langs,
+            "d_total_count": self.d_total_count,
+            "d_fork_count": self.d_fork_count,
+            "d_stargazer_count": self.d_stargazer_count,
+            "d_watcher_count": self.d_watcher_count,
+            "d_total_size": self.d_total_size,
+            "d_langs": self.d_langs,
         }
