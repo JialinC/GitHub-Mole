31a519076cd15fcb23068ed77d9904d83cee4455
JialinC <cjl742952519@gmail.com>
2024-04-01 23:04:38 -0400

    1. reformat code to align with PEP8 2. add documentation

--- backend/app/services/github_query/queries/costs/rate_limit.py
+++ backend/app/services/github_query/queries/costs/rate_limit.py
@@ -1,8 +1,12 @@
+"""The module defines the RateLimit class, which formulates the GraphQL query string
+to extract the remaining rate limit of the current user."""
+
 from backend.app.services.github_query.github_graphql.query import QueryNode, Query
+
 
 class RateLimit(Query):
     """
-    RateLimit is a subclass of Query designed to fetch information about the current rate limit status 
+    RateLimit is a subclass of Query designed to fetch information about the current rate limit status
     of the GitHub API, including the cost of the last query, remaining quota, and reset time.
     """
 
@@ -19,12 +23,12 @@ class RateLimit(Query):
                         "dryRun": "$dryrun"  # Indicates whether the rate limit should be checked in dry run mode.
                     },
                     fields=[
-                        "cost",      # The cost of the last query counted against the rate limit.
-                        "limit",     # The maximum number of points the client is permitted to consume in a window of time.
-                        "remaining", # The remaining number of points the client can consume.
-                        "resetAt",   # The time at which the current rate limit window resets in UTC epoch seconds.
-                        "used"       # The number of points used in the current rate limit window.
-                    ]
+                        "cost",  # The cost of the last query counted against the rate limit.
+                        "limit",  # The maximum number of points the client is permitted to consume in a window of time.
+                        "remaining",  # The remaining number of points the client can consume.
+                        "resetAt",  # The time at which the current rate limit window resets in UTC epoch seconds.
+                        "used",  # The number of points used in the current rate limit window.
+                    ],
                 )
             ]
         )
