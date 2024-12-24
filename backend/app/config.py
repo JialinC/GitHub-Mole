700a44f07ced9b9406e8daa88970ebebcc1a0ffd
JialinC <cjl742952519@gmail.com>
2024-12-23 21:50:29 -0500

    update app config

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,31 +2,43 @@
 GitHub's GraphQL API and a MySQL database. The configurations are divided into different classes, each serving a
 specific purpose, making the application modular, easy to manage, and scalable."""
 
-# import os
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 class Config:
     """The base configuration class that sets up essential configurations applicable across the application."""
 
-    DEBUG = True  # Ensure debug is enabled in your configuration for development
-    SECRET_KEY = "your_secret_key_here"  # Consider using environment variables
+    DEBUG = True
+    JWT_SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
+    # SESSION_COOKIE_HTTPONLY = True
+    # SESSION_COOKIE_SAMESITE = "Lax"
+    # SESSION_COOKIE_SECURE = False
+    FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 
 class AuthConfig(Config):
     """This class extends Config and includes configurations specific to GitHub OAuth authentication."""
 
-    GITHUB_OAUTH_CLIENT_ID = "918ef50cd94282d71b1b"
-    GITHUB_OAUTH_CLIENT_SECRET = "9d8242fe2293c6d8784f48ee6ba6117d1cb4e748"
+    GITHUB_CLIENT_ID = os.getenv("GITHUB_OAUTH_CLIENT_ID")
+    GITHUB_CLIENT_SECRET = os.getenv("GITHUB_OAUTH_CLIENT_SECRET")
+    GITHUB_AUTHORIZE_URL = f"{os.getenv('GITHUB_HOSTNAME')}/login/oauth/authorize"
+    GITHUB_ACCESS_TOKEN_URL = f"{os.getenv('GITHUB_HOSTNAME')}/login/oauth/access_token"
+    GITHUB_API_BASE_URL = os.getenv("GITHUB_API_URL")
 
 
 class DBConfig(Config):
     """This class, also extending Config, contains configurations necessary for connecting to and
     interacting with a MySQL database."""
 
-    MYSQL_DATABASE_USER = "tester"
-    MYSQL_DATABASE_PASSWORD = "password"
-    MYSQL_DATABASE_DB = "github_graphql"
-    MYSQL_DATABASE_HOST = "localhost"  # or your MySQL server address
+    MYSQL_DATABASE_USER = os.getenv("MYSQL_DATABASE_USER")
+    MYSQL_DATABASE_PASSWORD = os.getenv("MYSQL_DATABASE_PASSWORD")
+    MYSQL_DATABASE_DB = os.getenv("MYSQL_DATABASE_DB")
+    MYSQL_DATABASE_HOST = os.getenv("MYSQL_DATABASE_HOST")
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{MYSQL_DATABASE_USER}:"
         f"{MYSQL_DATABASE_PASSWORD}@{MYSQL_DATABASE_HOST}/"
