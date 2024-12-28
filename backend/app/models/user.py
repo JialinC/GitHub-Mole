e6149e03ccc1a547b25f2e2baf8470f7d8075b1b
JialinC <cjl742952519@gmail.com>
2024-12-28 13:31:51 -0500

    add user model and migration

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,17 +4,50 @@ from app.database import db
 
 
 class User(db.Model):
-    """
-    The User class is a data model that represents users within the application,
-    designed to work with a SQLAlchemy ORM as part of a Flask application.
-    This class provides a structure for storing user information,
-    including their username, email, and a token for GitHub authentication.
-    Additionally, it establishes a relationship with the GitHubUserData model,
-    allowing for the association of GitHub-specific metrics with each user.
-    """
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    github_token = db.Column(db.String(255), nullable=False)
-    github_data = db.relationship("GitHubUserData", backref="user", lazy="dynamic")
+    github_id = db.Column(db.String(80), unique=True, nullable=False)
+    github_login = db.Column(db.String(80), unique=True, nullable=False)
+    personal_access_token = db.Column(db.String(255), nullable=False)
+    api_url = db.Column(db.String(255), nullable=True)
+
+    queries = db.relationship(
+        "UserQuery", backref="user", lazy="dynamic", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"<User {self.github_id}>"
+
+    @classmethod
+    def create(cls, github_id, github_login, personal_access_token, api_url):
+        user = cls(
+            github_id=github_id,
+            github_login=github_login,
+            personal_access_token=personal_access_token,
+            api_url=api_url,
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    @classmethod
+    def read(cls, user_id):
+        return cls.query.get(user_id)
+
+    @classmethod
+    def update(cls, user_id, **kwargs):
+        user = cls.query.get(user_id)
+        if user:
+            for key, value in kwargs.items():
+                setattr(user, key, value)
+            db.session.commit()
+        return user
+
+    @classmethod
+    def delete(cls, user_id):
+        user = cls.query.get(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+        return user
