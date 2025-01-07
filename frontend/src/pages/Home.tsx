12d05c7b0bc8067fad876352102805bba72d1f25
JialinC <cjl742952519@gmail.com>
2025-01-07 00:27:51 -0500

    change bg pic

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -20,32 +20,35 @@ const Home: React.FC = () => {
           Login
         </button>
       </Navbar>
-      <main className="flex-grow container mx-auto p-4 flex flex-col items-center justify-center text-center">
-        <h1 className="text-4xl font-bold mb-4 text-white">
-          Welcome to GitHub-Mole
-        </h1>
-        <p className="text-lg text-white mb-4 max-w-xl mx-auto p-4 bg-opacity-75 bg-stone-900 rounded-lg shadow-lg">
-          This is an open-source APP designed to help you collect GitHub user
-          contribution stats and form SDE teams. For details, please check the
-          <a
-            href="https://github.com/yourusername/your-repo/blob/main/README.md"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-400 hover:text-blue-600 underline mx-1"
-          >
-            README
-          </a>
-          file and the
-          <a
-            href="https://yourdocumentationlink.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-400 hover:text-blue-600 underline mx-1"
-          >
-            DOC
-          </a>
-          . Click the 'Login' button in the top right corner to get started. 🌟
-        </p>
+      <main className="flex-grow container mx-auto p-4 flex flex-col items-center justify-start text-center mt-32">
+        <div className="bg-opacity-75 bg-stone-900 p-8 rounded-lg shadow-lg">
+          <h1 className="text-4xl font-bold mb-4 text-white">
+            Welcome to GitHub-Mole
+          </h1>
+          <p className="text-lg text-white mb-4 max-w-xl mx-auto">
+            This is an open-source APP designed to help you collect GitHub user
+            contribution stats and form SDE teams. For details, please check the
+            <a
+              href="https://github.com/yourusername/your-repo/blob/main/README.md"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-400 hover:text-blue-600 underline mx-1"
+            >
+              README
+            </a>
+            file and the
+            <a
+              href="https://yourdocumentationlink.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-400 hover:text-blue-600 underline mx-1"
+            >
+              DOC
+            </a>
+            . Click the 'Login' button in the top right corner to get started.
+            🌟
+          </p>
+        </div>
       </main>
       <Footer />
     </div>
