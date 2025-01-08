b4c6cc71e530b104482d3666c08f4efb8e05cca6
JialinC <cjl742952519@gmail.com>
2025-01-07 20:22:09 -0500

    update error page and error image

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import bg from "../assets/error.png";
 
 interface ErrorPageProps {
   message: string;
@@ -6,7 +7,10 @@ interface ErrorPageProps {
 
 const ErrorPage: React.FC<ErrorPageProps> = ({ message }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-red-100">
+    <div
+      className="flex items-center justify-center min-h-screen bg-red-100"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
       <div className="bg-white p-6 rounded shadow-md text-center">
         <h1 className="text-2xl font-bold text-red-600">Error</h1>
         <p className="mt-4 text-gray-700">{message}</p>
