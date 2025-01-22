037c0118a2f2a71b0fb2057bd2d1db61d59bdf44
JialinC <cjl742952519@gmail.com>
2025-01-21 23:35:28 -0500

    update gist comment model

--- backend/app/models/gist_comment.py
+++ backend/app/models/gist_comment.py
@@ -1,35 +1,42 @@
 from app.database import db
+from datetime import datetime
 
 
 class GistComment(db.Model):
     __tablename__ = "gist_comments"
 
     id = db.Column(db.Integer, primary_key=True)
-    comment_id = db.Column(db.String(80), nullable=False)
     body_text = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=True)
     author_github_login = db.Column(db.String(80), nullable=False)
     user_query_id = db.Column(
-        db.Integer, db.ForeignKey("user_queries.id"), nullable=False
+        db.Integer, db.ForeignKey("user_queries.id", ondelete="CASCADE"), nullable=False
     )
 
     def __repr__(self):
-        return f"<GistComment {self.comment_id}>"
+        return f"<GistComment {self.body_text}>"
 
     @classmethod
-    def create(
-        cls, comment_id, body_text, created_at, author_github_login, user_query_id
-    ):
+    def create(cls, body_text, created_at, author_github_login, user_query_id):
         gist_comment = cls(
-            comment_id=comment_id,
             body_text=body_text,
             created_at=created_at,
             author_github_login=author_github_login,
             user_query_id=user_query_id,
         )
-        db.session.add(gist_comment)
-        db.session.commit()
         return gist_comment
+
+    @classmethod
+    def create_from_row(cls, row, user_query_id):
+        created_at = (
+            None if row[1] == "N/A" else datetime.strptime(row[1], "%Y-%m-%dT%H:%M:%SZ")
+        )
+        return cls(
+            body_text=row[2],
+            created_at=created_at,
+            author_github_login=row[0],
+            user_query_id=user_query_id,
+        )
 
     @classmethod
     def read(cls, id):
