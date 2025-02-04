edaeb99248f6f8ae55a75e8593f6f53c428ffd1f
JialinC <cjl742952519@gmail.com>
2025-02-03 22:48:40 -0500

    update button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface ButtonProps {
   handleAction: () => void;
   text: string;
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ handleAction, text }) => {
+const Button: React.FC<ButtonProps> = ({ handleAction, text, disabled }) => {
   return (
     <div className="mt-6">
       <button
+        {...(disabled !== undefined && { disabled })}
         onClick={handleAction}
         className="w-full bg-red-600 hover:bg-red-800 text-white font-semibold py-3 rounded-lg shadow-md transition duration-200"
       >
