ce4d4be627a6de6faaa21be6644e650546c92f7e
JialinC <cjl742952519@gmail.com>
2025-02-08 19:31:03 -0500

    change baseURL for deployment

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5000'
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:5000'
 });
 
 // Request interceptor to add the access token to headers
